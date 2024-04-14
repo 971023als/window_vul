7a25411603191736e3f108a3975ef9f24da33664
971023als <971023als@naver.com>
2024-04-15 02:23:10 +0900

    Update W-34.ps1

--- powershell/W-34.ps1
+++ powershell/W-34.ps1
@@ -9,26 +9,15 @@ $auditParams = @{
     Mitigation = "Use of non-decryptable encryption for password storage"
 }
 
-# Request Administrator privileges if not already running with them
-if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Start-Process PowerShell.exe -ArgumentList "-NoProfile", "-ExecutionPolicy Bypass", "-File", "`"$PSCommandPath`"", "-Verb", "RunAs"
-    exit
-}
 
-$computerName = $env:COMPUTERNAME
-$dirs = @{
-    Raw = "C:\Window_${computerName}_raw"
-    Result = "C:\Window_${computerName}_result"
-}
-
-# Prepare environment
+# 환경 설정
 function Initialize-Environment {
     chcp 437 | Out-Null
     $host.UI.RawUI.BackgroundColor = "DarkGreen"
     $host.UI.RawUI.ForegroundColor = "Green"
     Clear-Host
 
-    Write-Host "Setting up the environment..."
+    Write-Host "환경을 설정하고 있습니다..."
     Remove-Item -Path $dirs.Raw, $dirs.Result -Recurse -Force -ErrorAction SilentlyContinue
     New-Item -Path $dirs.Raw, $dirs.Result -ItemType Directory | Out-Null
 
@@ -38,24 +27,24 @@ function Initialize-Environment {
     systeminfo | Out-File -FilePath "$($dirs.Raw)\systeminfo.txt"
 }
 
-# IIS Configuration Analysis
+# IIS 구성 분석
 function Analyze-IISConfiguration {
-    Write-Host "Analyzing IIS Settings..."
+    Write-Host "IIS 설정을 분석하고 있습니다..."
     $applicationHostConfig = Get-Content "$env:WinDir\System32\Inetsrv\Config\applicationHost.Config"
     $applicationHostConfig | Out-File -FilePath "$($dirs.Raw)\iis_setting.txt"
 
     if ($applicationHostConfig -match "IIS5") {
-        $auditParams.CurrentStatus += "Deprecated IIS version detected. Upgrade required."
-        $auditParams.AuditResult = "Vulnerable"
+        $auditParams.CurrentStatus += "사용되지 않는 IIS 버전이 감지되었습니다. 업그레이드가 필요합니다."
+        $auditParams.AuditResult = "취약"
     } else {
-        $auditParams.CurrentStatus += "No deprecated IIS version detected. Compliant with security standards."
+        $auditParams.CurrentStatus += "사용되지 않는 IIS 버전이 감지되지 않았습니다. 보안 표준을 준수하고 있습니다."
     }
 }
 
-# Execute the script steps
+# 스크립트 단계 실행
 Initialize-Environment
 Analyze-IISConfiguration
 
-# Save the JSON results to a file
+# JSON 결과 파일 저장
 $jsonFilePath = "$($dirs.Result)\W-34.json"
 $auditParams | ConvertTo-Json -Depth 3 | Out-File -FilePath $jsonFilePath
