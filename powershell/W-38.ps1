1c4618f8cef695019cf1ff0786da6169abe8f410
971023als <971023als@naver.com>
2024-04-15 00:16:05 +0900

    Update W-38.ps1

--- powershell/W-38.ps1
+++ powershell/W-38.ps1
@@ -1,22 +1,21 @@
 # Convert your existing hashtable to a PSCustomObject for easier manipulation
 $json = [PSCustomObject]@{
-        "분류" = "서비스관리"
-        "코드" = "W-38"
-        "위험도" = "상"
-        "진단 항목" = "FTP 디렉토리 접근권한 설정"
-        "진단 결과" = "양호"  # 기본 값을 "양호"로 가정
-        "현황" = @()
-        "대응방안" = "FTP 디렉토리 접근권한 설정"
-    }
+    "분류" = "서비스관리"
+    "코드" = "W-38"
+    "위험도" = "상"
+    "진단 항목" = "FTP 디렉토리 접근권한 설정"
+    "진단 결과" = "양호"  # 기본 값을 "양호"로 가정
+    "현황" = @()
+    "대응방안" = "FTP 디렉토리 접근권한 설정"
+}
 
-# 관리자 권한 요청
-If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    $arguments = "-NoProfile", "-ExecutionPolicy Bypass", "-File", "`"$PSCommandPath`"", "$args"
-    Start-Process "PowerShell" -Verb RunAs -ArgumentList $arguments
+# Request Administrator privileges
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -ArgumentList "-NoProfile", "-ExecutionPolicy Bypass", "-File", "`"$PSCommandPath`"" -Verb RunAs
     Exit
 }
-# Configuration and setup code remains as you have provided.
 
+# Configuration and setup code remains as you have provided.
 Write-Host "------------------------------------------W-38 점검 시작------------------------------------------"
 
 $isSecure = $true # Default to true assuming secure until proven otherwise
@@ -27,7 +26,7 @@ If (Test-Path "$rawDir\FTP_PATH.txt") {
         $filePath = $_
         $acl = Get-Acl $filePath
         $hasFullControl = $acl.Access | Where-Object {
-            $_.FileSystemRights -like "*FullControl*" -and $_.IdentityReference -like "*Everyone*"
+            $_.FileSystemRights -match "FullControl" -and $_.IdentityReference -eq "Everyone"
         }
 
         # Update $isSecure based on the diagnostics result
@@ -46,7 +45,6 @@ if ($isSecure) {
     $json.현황 += "EVERYONE 그룹에 대한 FullControl 접근 권한이 발견되어 취약합니다."
 }
 
-# JSON 결과를 파일에 저장
+# Save JSON results to a file
 $jsonFilePath = "$resultDir\W-38.json"
 $json | ConvertTo-Json -Depth 3 | Out-File -FilePath $jsonFilePath
-
