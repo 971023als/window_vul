d858d6ad8a30748f5f0211b0f1e440cd29cc9618
971023als <971023als@naver.com>
2024-04-15 00:35:20 +0900

    Update W-51.ps1

--- powershell/W-51.ps1
+++ powershell/W-51.ps1
@@ -11,20 +11,20 @@ $json = @{
 
 # 관리자 권한 요청
 If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Start-Process PowerShell.exe -ArgumentList "Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -File `"$PSCommandPath`"' -Verb RunAs"
+    Start-Process PowerShell.exe -ArgumentList "-NoProfile", "-ExecutionPolicy Bypass", "-File", "$PSCommandPath", "-Verb", "RunAs"
     exit
 }
 
 # 콘솔 환경 설정
-chcp 437 > $null
+chcp 437 | Out-Null
 $Host.UI.RawUI.BackgroundColor = "DarkGreen"
 $Host.UI.RawUI.ForegroundColor = "White"
 Clear-Host
 
 # 기본 설정
 $computerName = $env:COMPUTERNAME
-$rawDir = "C:\Window_$($computerName)_raw"
-$resultDir = "C:\Window_$($computerName)_result"
+$rawDir = "C:\Window_${computerName}_raw"
+$resultDir = "C:\Window_${computerName}_result"
 
 # 디렉터리 준비
 Remove-Item -Path $rawDir, $resultDir -Recurse -Force -ErrorAction SilentlyContinue
@@ -36,7 +36,7 @@ $telnetServiceStatus = Get-Service -Name TlntSvr -ErrorAction SilentlyContinue
 
 If ($telnetServiceStatus -and $telnetServiceStatus.Status -eq 'Running') {
     Try {
-        $telnetConfig = & tlntadmn config
+        $telnetConfig = & tlntadmn.exe config
         $authenticationMethod = $telnetConfig | Where-Object {$_ -match "Authentication"}
 
         If ($authenticationMethod -match "NTLM" -and $authenticationMethod -notmatch "Password") {
@@ -51,6 +51,7 @@ If ($telnetServiceStatus -and $telnetServiceStatus.Status -eq 'Running') {
         $json.현황 += "Telnet 서비스 설정을 검색하는 데 실패했습니다."
     }
 } Else {
+    $json.진단 결과 = "양호"
     $json.현황 += "Telnet 서비스가 실행되지 않거나 설치되지 않았으며, 이는 안전으로 간주됩니다."
 }
 
@@ -59,12 +60,12 @@ Write-Host "------------------------------------------End of Telnet Service Secu
 # JSON 결과를 파일에 저장
 $jsonFilePath = "$resultDir\W-51.json"
 $json | ConvertTo-Json -Depth 3 | Out-File -FilePath $jsonFilePath
-Write-Host "진단 결과가 저장되었습니다: $jsonPath"
+Write-Host "진단 결과가 저장되었습니다: $jsonFilePath"
 
 # 결과 요약 및 저장
 Get-Content "$resultDir\W-51_${computerName}_diagnostic_results.json" | Out-File "$resultDir\security_audit_summary.txt"
 
 # 정리 작업
-Remove-Item -Path $rawDir\* -Force
+Remove-Item -Path "$rawDir\*" -Force
 
 Write-Host "Script has completed. Results have been saved to $resultDir\security_audit_summary.txt."
