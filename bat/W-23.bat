1b35dd7a89edf60840b7c7071d354840fdea8e4a
971023als <971023als@naver.com>
2024-04-20 23:16:11 +0900

    Update W-23.bat

---
+++ bat/W-23.bat
@@ -0,0 +1,49 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+
+:: 관리자 권한으로 실행 확인
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    powershell -Command "Start-Process cmd -ArgumentList '/c %~0' -Verb RunAs"
+    exit
+)
+
+:: 기본 설정
+set "computerName=%COMPUTERNAME%"
+set "rawPath=C:\Window_%computerName%_raw"
+set "resultPath=C:\Window_%computerName%_result"
+
+:: 디렉토리 초기화 및 생성
+if exist "%rawPath%" rmdir /s /q "%rawPath%"
+if exist "%resultPath%" rmdir /s /q "%resultPath%"
+mkdir "%rawPath%"
+mkdir "%resultPath%"
+
+:: 보안 정책 파일 내보내기 및 시스템 정보 수집
+secedit /export /cfg "%rawPath%\Local_Security_Policy.txt"
+systeminfo > "%rawPath%\systeminfo.txt"
+
+:: IIS 설정 파일 분석
+set "iisConfigFile=%env:WinDir%\System32\Inetsrv\Config\applicationHost.Config"
+if exist "!iisConfigFile!" (
+    type "!iisConfigFile!" > "%rawPath%\iis_setting.txt"
+    for /f "delims=" %%a in ('type "%rawPath%\iis_setting.txt" ^| findstr /I "directoryBrowse enabled"') do (
+        set "dirBrowseEnabled=%%a"
+        if "!dirBrowseEnabled!"=="<directoryBrowse enabled=\"true\"" (
+            set "directoryStatus=Enabled"
+        ) else (
+            set "directoryStatus=Disabled"
+        )
+    )
+)
+
+:: 진단 결과 CSV 파일로 저장
+echo 분류,코드,위험도,진단항목,진단결과,현황,대응방안 > "%resultPath%\W-23.csv"
+if "!directoryStatus!"=="Enabled" (
+    echo 서비스관리,W-23,상,IIS 디렉토리 리스팅 제거,취약,디렉토리 브라우징 활성화,IIS 디렉토리 리스팅 제거 필요 >> "%resultPath%\W-23.csv"
+) else (
+    echo 서비스관리,W-23,상,IIS 디렉토리 리스팅 제거,양호,디렉토리 브라우징 비활성화,추가 조치 필요 없음 >> "%resultPath%\W-23.csv"
+)
+
+:: 스크립트 실행 완료 메시지
+echo 스크립트 실행 완료
